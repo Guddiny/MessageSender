cf1eb03613f1e3abac78a21a5a48d4e3902c2f0e
AleksandrLiakhavetsEPAM <Aliaksandr.Liakhavets@resideo.com>
2026-05-08 21:19:03 +0300

    Update avalonia nad .net related packages

--- MessageSender/App.axaml.cs
+++ MessageSender/App.axaml.cs
@@ -47,10 +47,6 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        // Line below is needed to remove Avalonia data validation.
-        // Without this line you will get duplicate validations from both Avalonia and CT
-        BindingPlugins.DataValidators.RemoveAt(0);
-
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             _mainViewModel = Ioc.Default.GetService<MainViewModel>()!;
